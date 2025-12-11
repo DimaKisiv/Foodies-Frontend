5c2b587f23ba979b6e9aa3a4f8a6a4a72e0e92f2
Volodymyr Vladykin <vova@pocketguard.com>
2025-12-12 00:49:43 +0200

    Implement most styling for RecipeInfo component

--- src/components/RecipeInfo/RecipeInfo.jsx
+++ src/components/RecipeInfo/RecipeInfo.jsx
@@ -1,39 +1,70 @@
+import styles from "./RecipeInfo.module.css";
+
 const RecipeInfo = ({ recipe }) => {
-    const handleFavoritesChange = () => {};
-    
-    return (
-         <>
-          <img src={recipe.thumb} width="343" height="318" />
-          <h3>{recipe.title}</h3>
-          <ul>
-            <li>{recipe.category}</li>
-            <li>{recipe.time} min</li>
-          </ul>
-          <p>{recipe.description}</p>
-          <div>
-            <p>Created by:</p>
-            <p>{recipe.owner.name}</p>
-          </div>
+  const handleFavoritesChange = () => {};
 
-          <h3>Ingredients</h3>
-          <ul>
-            {recipe.ingredientsDetailed.map((ingredient) => {
-              return (
-                <div>
-                  <img src={ingredient.img} width="55" height="55" />
-                  <p>{ingredient.name}</p>
-                  <p>{ingredient.measure}</p>
-                </div>
-              );
-            })}
-          </ul>
+  return (
+    <div className={styles.recipe}>
+      <img className={styles["thumb"]} src={recipe.thumb} />
 
-          <h3>Recipe Preparation</h3>
-          <p>{recipe.instructions}</p>
+      <h3 className={styles["section-header"]}>{recipe.title}</h3>
+      <ul className={styles["tags-list"]}>
+        <li key="category" className={styles.tag}>
+          {recipe.category}
+        </li>
+        <li key="time" className={styles.tag}>
+          {recipe.time} min
+        </li>
+      </ul>
 
-          <button onClick={handleFavoritesChange}>ADD TO FAVORITES</button>
-        </>
-    );
+      <p className={styles.description}>{recipe.description}</p>
+
+      <RecipeAuthor author={recipe.owner} />
+
+      <h3 className={styles["section-header"]}>Ingredients</h3>
+      <div className={styles["ingredients-grid"]}>
+        {recipe.ingredientsDetailed.map((ingredient) => {
+          return (
+            <div key={ingredient.id} className={styles.ingredient}>
+              <img src={ingredient.img} width="55" height="55" />
+              <div>
+                <p className={styles["ingredient-name"]}>{ingredient.name}</p>
+                <p className={styles["ingredient-measure"]}>
+                  {ingredient.measure}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <h3 className={styles["section-header"]}>Recipe Preparation</h3>
+      <p className={styles.description}>{recipe.instructions}</p>
+
+      {/* TODO change text and probably styles when recipe is favorite */}
+      <button
+        className={styles["favorites-button"]}
+        onClick={handleFavoritesChange}
+      >
+        Add to favorites
+      </button>
+    </div>
+  );
+};
+
+// /avatars/3fc594ab58eb619b041eb72d_1765489692581.jpeg
+// https://www.gravatar.com/avatar/b9e75194e4348c430fe0e4d5f7d900fd
+const RecipeAuthor = ({ author }) => {
+  // TODO handling for empty/invalid URL
+  return (
+    <div className={styles.author}>
+      <img className={styles["author-avatar"]} src={author.avatar} />
+      <div className={styles["author-name-layout"]}>
+        <p className={styles["author-created-by-text"]}>Created by:</p>
+        <p className={styles["author-name"]}>{author.name}</p>
+      </div>
+    </div>
+  );
 };
 
 export default RecipeInfo;
