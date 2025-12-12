d37c524386575b4dada6cf3df7cb359ca1583337
illia.artiushenko <iartiushenko@deloitte.com>
2025-12-12 17:50:35 +0200

    made integration with BE(auth)

--- src/components/LogOutModal/LogOutModal.jsx
+++ src/components/LogOutModal/LogOutModal.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import css from "./LogOutModal.module.css"
 import Modal from "../Modal/Modal.jsx";
+import { useDispatch } from "react-redux";
+import { logout } from "../../redux/auth/authOperations.js";
 
 function LogOutModal({ isOpen, onClose }) {
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(logout());
+    };
 
     return (
         <Modal className={css['logOut-modal']} isOpen={isOpen} onClose={onClose}>
@@ -11,7 +20,7 @@ function LogOutModal({ isOpen, onClose }) {
                 <p className={css['modal-description']}>You can always log back in at my time.</p>
 
                 <div className={css['modal-actions']}>
-                    <button className={css['modal-btn']}>Log Out</button>
+                    <button className={css['modal-btn']} onClick={handleSubmit}>Log Out</button>
                     <button className={css['modal-btn__outline']} onClick={onClose}>Cancel</button>
                 </div>
             </div>
