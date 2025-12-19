a926be027822ab3e7d4094561e72a84912be9606
dimakisiv <dimakisiv@gmail.com>
2025-12-19 16:08:46 +0200

    favorite button toggle on RecipeCard

--- src/components/HomePage/Recipes/RecipeCard/RecipeCard.jsx
+++ src/components/HomePage/Recipes/RecipeCard/RecipeCard.jsx
@@ -1,16 +1,38 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { selectIsAuthenticated } from "../../../../redux/auth/authSlice.js";
 import Avatar from "boring-avatars";
-import RoundButton from "../../../shared/Button/RoundButton/RoundButton.jsx";
+import RoundButton from "../../../Shared/Button/RoundButton/RoundButton.jsx";
+import { useAuthModal } from "../../../../providers/useAuthModal";
+import { selectFavoritesRecipes } from "../../../../redux/recipes/recipesSlice";
+import {
+  addRecipeToFavorites,
+  deleteRecipeFromFavorites,
+} from "../../../../redux/recipes/recipesOperations";
 import css from "./RecipeCard.module.css";
 
 // TODO: avatars
 const RecipeCard = ({ recipe }) => {
+  const dispatch = useDispatch();
   const isUserSignedIn = useSelector(selectIsAuthenticated);
+  const favorites = useSelector(selectFavoritesRecipes) || [];
   const navigate = useNavigate();
+  const { openSignIn } = useAuthModal();
 
-  const favoritesHandler = () => {}
+  const isFavorite = favorites.some((r) => String(r.id) === String(recipe.id));
+
+  const favoritesHandler = (e) => {
+    e?.preventDefault?.();
+    if (!isUserSignedIn) {
+      openSignIn();
+      return;
+    }
+    if (isFavorite) {
+      dispatch(deleteRecipeFromFavorites(recipe.id));
+    } else {
+      dispatch(addRecipeToFavorites(recipe.id));
+    }
+  };
 
   const recipeHandler = () => navigate(`/recipe/${recipe.id}`);
 
@@ -26,9 +48,11 @@ const RecipeCard = ({ recipe }) => {
       <div className={css.ui}>
         <div className={css.author}>
           <div className={css.avatar}>
-            {recipe.owner.avatar
-              ? <img src={recipe.owner.avatar} alt={recipe.owner.name} />
-              : <Avatar name={recipe.owner.name} size={40} variant="pixel" />}
+            {recipe.owner.avatar ? (
+              <img src={recipe.owner.avatar} alt={recipe.owner.name} />
+            ) : (
+              <Avatar name={recipe.owner.name} size={40} variant="pixel" />
+            )}
           </div>
           <span>{recipe.owner.name}</span>
         </div>
@@ -36,12 +60,9 @@ const RecipeCard = ({ recipe }) => {
           <RoundButton
             iconId="icon-heart"
             onClick={favoritesHandler}
-            altMode={false}
+            altMode={isFavorite}
           />
-          <RoundButton
-            iconId="icon-arrow-up-right"
-            onClick={recipeHandler}
-          />
+          <RoundButton iconId="icon-arrow-up-right" onClick={recipeHandler} />
         </div>
       </div>
     </div>
