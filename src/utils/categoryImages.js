09fcd813209717df06816568374d702d2d4da879
dimakisiv <dimakisiv@gmail.com>
2025-12-20 22:53:36 +0200

    fix domain url for category images

--- src/utils/categoryImages.js
+++ src/utils/categoryImages.js
@@ -1,29 +1,30 @@
-const API_URL = 'http://localhost:3000/';
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 
 const categoryFileNames = {
-  "Beef": "beef.jpg",
-  "Breakfast": "breakfast.jpg",
-  "Chicken": "chicken.jpg",
-  "Dessert": "desserts.jpg",
-  "Goat": "goat.jpg",
-  "Lamb": "lamb.jpg",
-  "Miscellaneous": "miscellaneous.jpg",
-  "Pasta": "pasta.jpg",
-  "Pork": "pork.jpg",
-  "Seafood": "seafood.jpg",
-  "Side": "side.jpg",
-  "Soup": "soup.jpg",
-  "Starter": "starter.jpg",
-  "Vegan": "vegan.jpg",
-  "Vegetarian": "vegetarian.jpg",
+  Beef: "beef.jpg",
+  Breakfast: "breakfast.jpg",
+  Chicken: "chicken.jpg",
+  Dessert: "desserts.jpg",
+  Goat: "goat.jpg",
+  Lamb: "lamb.jpg",
+  Miscellaneous: "miscellaneous.jpg",
+  Pasta: "pasta.jpg",
+  Pork: "pork.jpg",
+  Seafood: "seafood.jpg",
+  Side: "side.jpg",
+  Soup: "soup.jpg",
+  Starter: "starter.jpg",
+  Vegan: "vegan.jpg",
+  Vegetarian: "vegetarian.jpg",
 };
 
 export const getCategoryImage = (categoryName) => {
+  const backendDomain = BASE_URL.replace(/\/api\/?$/, "");
   const fileName = categoryFileNames[categoryName];
 
   if (!fileName) {
-    return `${API_URL}/categories/beef.jpg`;
+    return `${backendDomain}/categories/beef.jpg`;
   }
 
-  return `${API_URL}/categories/${fileName}`;
+  return `${backendDomain}/categories/${fileName}`;
 };
