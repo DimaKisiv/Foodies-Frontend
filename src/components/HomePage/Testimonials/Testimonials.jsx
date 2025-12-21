0a11da3a15f512e60c8ac318581859e30c578a7c
Yuriy Borys <iam618384@gmail.com>
2025-12-21 14:46:20 +0200

    Updated testimonials

--- src/components/HomePage/Testimonials/Testimonials.jsx
+++ src/components/HomePage/Testimonials/Testimonials.jsx
@@ -1,4 +1,27 @@
-import styles from "./Testimonials.module.css";
-export default function Testimonials() {
-  return <div>Testimonials</div>;
+import { useSelector } from "react-redux";
+import { selectTestimonialsItems, selectTestimonialsStatus } from "../../../redux/testimonials/testimonialsSlice.js";
+import Loader from "../../shared/Loader/Loader.jsx";
+import MainTitle from "../../shared/MainTitle/MainTitle.jsx";
+import Subtitle from "../../shared/Subtitle/Subtitle.jsx";
+import TestimonialsCarousel from "./TestimonialsCarousel/TestimonialsCarousel.jsx";
+import css from "./Testimonials.module.css";
+
+const Testimonials = () => {
+  const testimonials = useSelector(selectTestimonialsItems);
+  const testimonialsStatus = useSelector(selectTestimonialsStatus);
+
+  return (
+    <section className={css.testimonials}>
+      <div className={css.testimonialsWrapper}>
+        <Subtitle>What our customer say</Subtitle>
+        <MainTitle>Testimonials</MainTitle>
+        {testimonialsStatus === "loading" && <Loader/>}
+        {testimonials.length > 0 && (
+          <TestimonialsCarousel slideList={testimonials}/>
+        )}
+      </div>
+    </section>
+  );
 }
+
+export default Testimonials;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,24 +1,16 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import { fetchPopularRecipes } from "../../redux/recipes/recipesOperations";
 import { fetchCategories } from "../../redux/categories/categoriesOperations";
-import Loader from "../../components/shared/Loader/Loader";
+import { fetchTestimonials } from "../../redux/testimonials/testimonialsOperations.js";
 import Hero from "../../components/HomePage/Hero/Hero";
 import Categories from "./Categories/Categories.jsx";
 import Recipes from "./Recipes/Recipes.jsx";
-import {
-  selectTestimonialsItems,
-  selectTestimonialsStatus,
-} from "../../redux/testimonials/testimonialsSlice.js";
-import { fetchTestimonials } from "../../redux/testimonials/testimonialsOperations.js";
-import TestimonialsCarousel from "../../components/HomePage/Testimonials/TestimonialsCarousel/TestimonialsCarousel.jsx";
-import css from "./HomePage.module.css";
+import Testimonials from "../../components/HomePage/Testimonials/Testimonials.jsx";
 
 const HomePage = () => {
   const dispatch = useDispatch();
-  const testimonials = useSelector(selectTestimonialsItems);
-  const testimonialsStatus = useSelector(selectTestimonialsStatus);
 
   useEffect(() => {
     dispatch(fetchPopularRecipes());
@@ -34,16 +26,7 @@ const HomePage = () => {
       <Hero />
       {/* If category query present, show recipes; otherwise categories */}
       {categoryName ? <Recipes /> : <Categories />}
-      <section className={css.testimonials}>
-        <div className={css.testimonialsWrapper}>
-          <h5>What our customer say</h5>
-          <h2>Testimonials</h2>
-          {testimonialsStatus === "loading" && <Loader />}
-          {testimonials.length > 0 && (
-            <TestimonialsCarousel slideList={testimonials} />
-          )}
-        </div>
-      </section>
+      <Testimonials />
     </main>
   );
 };
